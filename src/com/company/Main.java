7ebd315d77e64fbb1371beeb16866d69a7459ff6
phili <philip.clamon@gmail.com>
2019-01-27 20:31:09 -0600

    Initial check in2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,40 +6,45 @@ public class Main
     {
         java.util.Scanner userInput = new java.util.Scanner(System.in);
 
-        System.out.println("Cheese diameter: ");
-        int diam = userInput.nextInt();
-
-        switch (diam)
+        while (true)
         {
-            case (1):
-                diam = 1;
-                break;
-            case (2):
-                diam = 2;
-                break;
-            case (3):
-                diam = 3;
-                break;
-            default:
-                System.out.println("That order is TOO crazy.");
-        }
+            System.out.println("Cheese diameter: ");
+            int diam = userInput.nextInt();
 
-        System.out.println("Length: ");
-        int length = userInput.nextInt();
+            switch (diam)
+            {
+                case (1):
+                    diam = 1;
+                    break;
+                case (2):
+                    diam = 2;
+                    break;
+                case (3):
+                    diam = 3;
+                    break;
+                default:
+                    System.out.println("That order is TOO crazy.");
+            }
 
-        if (length < 1)
-        {
-            System.out.println("That order is not crazy enough.");
+            System.out.println("Length: ");
+            int length = userInput.nextInt();
+
+            if (length < 1)
+            {
+                System.out.println("That order is not crazy enough.");
+            }
+
+            System.out.println("Total is: " + calculateOrder(length, diam));
         }
     }
 
-    public static int calculateOrder(int length, int diam)
+    private static int calculateOrder(int length, int diam)
     {
         int price = 0;
         int shipping = 0;
         int order;
         int handlingCharge = 5;
-        boolean freeShipping = false;
+        boolean freeShipping = (diam == 1 && length > 50) || (diam == 2 && length > 75) || (diam == 3 && length > 25);
 
         switch (diam)
         {
@@ -64,6 +69,20 @@ public class Main
                 shipping = 4;
                 break;
         }
+
+        if (freeShipping)
+        {
+            shipping = 0;
+        }
+        else if (freeShipping)
+        {
+            shipping = 0;
+        }
+        else if (freeShipping)
+        {
+            shipping = 0;
+        }
+
         order = handlingCharge + shipping * length + price * length;
 
         return order;
